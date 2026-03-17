25a1d4934386fe940deae7191f1eb92d09792f40
Alyssa Cox <cox24018@byui.edu>
2026-03-17 13:21:38 -0600

    fixed failure in mortality_multiple_2020

--- inst/data_mortality_multiple_2020.r
+++ inst/data_mortality_multiple_2020.r
@@ -123,6 +123,7 @@ data_mortality_multiple_2020 <- tribble(
 
   # ── Race recode 40 (positions 489-490) ───────────────────────────────────────
   "race_recode_40",             489,  490,    2,  "int",  "Race recode 40 (multiple-race combinations)",          "01=White|02=Black|03=American Indian or Alaskan Native (AIAN)|04=Asian Indian|05=Chinese|06=Filipino|07=Japanese|08=Korean|09=Vietnamese|10=Other or Multiple Asian|11=Hawaiian|12=Guamanian|13=Samoan|14=Other or Multiple Pacific Islander|15=Black and White|16=Black and AIAN|17=Black and Asian|18=Black and Native Hawaiian or Other Pacific Islander (NHOPI)|19=AIAN and White|20=AIAN and Asian|21=AIAN and NHOPI|22=Asian and White|23=Asian and NHOPI|24=NHOPI and White|25=Black, AIAN and White|26=Black, AIAN and Asian|27=Black, AIAN and NHOPI|28=Black, Asian and White|29=Black, Asian and NHOPI|30=Black, NHOPI and White|31=AIAN, Asian and White|32=AIAN, NHOPI and White|33=AIAN, Asian and NHOPI|34=Asian, NHOPI and White|35=Black, AIAN, Asian and White|36=Black, AIAN, Asian and NHOPI|37=Black, AIAN, NHOPI and White|38=Black, Asian, NHOPI and White|39=AIAN, Asian, NHOPI and White|40=Black, AIAN, Asian, NHOPI and White|99=Unknown and Other Race",
+  "reserved_13",                491,  805,   315,  "str",  "Reserved positions",                                   "",
 
   # ── Decedent's usual occupation and industry (positions 806-817) ─────────────
   # NEW in 2020. 46 states/jurisdictions participating; AZ, IA, NC, RI, DC excluded.
